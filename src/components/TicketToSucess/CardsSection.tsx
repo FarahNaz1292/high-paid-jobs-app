5c0545bcee2072177d62ce7362e8fe04136257ef
Farah naz <farahnaz@Mac.lan>
2025-03-18 01:34:09 -0400

    Completed Todays assigned tasks

--- src/components/TicketToSucess/CardsSection.tsx
+++ src/components/TicketToSucess/CardsSection.tsx
@@ -1,7 +1,6 @@
 'use client'
 import {
     Card,
-    CardContent,
     CardDescription,
     CardHeader,
     CardTitle,
@@ -20,10 +19,78 @@ import LiveInterview from "../../../public/liveInterview.svg"
 
 import React from 'react'
 
+
+
 const CardsSection = () => {
     return (
         <>
+            <div className="grid grid-cols-3 gap-5 container mx-auto">
+                <Card>
+                    <CardHeader>
+                        <CardTitle className="flex justify-center items-center">
+                            <Image src={JobIcon} alt="Icon" />
+                        </CardTitle>
+                        <CardDescription className="text-nowrap font-bold text-2xl text-black">99.99% Guaranteed Job<br />Placement</CardDescription>
+                        <p className="text-nowrap  text-[#5C5958] dark:text-gray">Land a $100k+ job in 8-16 weeks—or we <br /> keep working until you do</p>
+                    </CardHeader>
+
+                </Card>
+                <Card >
+                    <CardHeader>
+                        <CardTitle className="flex justify-center items-center">
+                            <Image src={recessionProof} alt="Icon" />
+                        </CardTitle>
+                        <CardDescription className="text-nowrap font-bold text-2xl text-black">AI & Recession-Proof<br />Careers</CardDescription>
+                        <p className="text-nowrap  text-[#5C5958] dark:text-gray">Future-ready roles that thrive no matter <br />the economy.</p>
+                    </CardHeader>
+
+                </Card>
+                <Card >
+                    <CardHeader>
+                        <CardTitle className="flex justify-center items-center">
+                            <Image src={LiveInterview} alt="Icon" />
+                        </CardTitle>
+                        <CardDescription className="text-nowrap font-bold text-2xl text-black">Live Interview<br />Assistance</CardDescription>
+                        <p className="text-nowrap  text-[#5C5958] dark:text-gray">Our experts join your interviews to help<br /> you shine</p>
+                    </CardHeader>
+
+                </Card>
+                <Card >
+                    <CardHeader>
+                        <CardTitle className="flex justify-center items-center">
+                            <Image src={Mentoring} alt="Icon" />
+                        </CardTitle>
+                        <CardDescription className="text-nowrap font-bold text-2xl text-black">One-on-One Mentoring</CardDescription>
+                        <p className="text-nowrap  text-[#5C5958] dark:text-gray">Personalized guidance tailored to your <br />goals</p>
+                    </CardHeader>
+
+                </Card>
+                <Card >
+                    <CardHeader>
+                        <CardTitle className="flex justify-center items-center">
+                            <Image src={Salary} alt="Icon" />
+                        </CardTitle>
+                        <CardDescription className="text-nowrap font-bold text-2xl text-black">Salary Negotiation Boost</CardDescription>
+                        <p className="text-nowrap  text-[#5C5958] dark:text-gray">Secure an extra $5k-$10k on your offer<br /> —guaranteed.
+
+                        </p>
+                    </CardHeader>
+
+                </Card>
+                <Card >
+                    <CardHeader>
+                        <CardTitle className="flex justify-center items-center">
+                            <Image src={Probation} alt="Icon" />
+                        </CardTitle>
+                        <CardDescription className="text-nowrap font-bold text-2xl text-black">Probation Period <br />Support</CardDescription>
+                        <p className="text-nowrap  text-[#5C5958] dark:text-gray">
+                            We're with you for your first 3-6 months<br />on the job.</p>
+                    </CardHeader>
+
+                </Card>
+            </div>
            
+
         </>
 
     )
--- src/components/TicketToSucess/TicketToSucess.tsx
+++ src/components/TicketToSucess/TicketToSucess.tsx
@@ -1,25 +1,31 @@
 'use client'
-
-
-
-
-
 import React from 'react'
 import CardsSection from './CardsSection'
+import { Button } from '../ui/button'
+import { Link } from 'lucide-react'
+import Arrow from "../../../public/arrow.svg"
+import Image from 'next/image'
 
 const TicketToSucess = () => {
   return (
-  <>
-   <div className=''>
-    <div className='mx-auto text-center '>
-        <h1 className='max-w-[320px] md:max-w-[739px] mx-auto text-2xl md:text-5xl font-bold text-black leading-tight mb-2 md:mb-4'>Why High Paid Jobs is your ticket <br/>to success</h1>
-        <p className='text-[#5C5958] max-w-[270px] mx-auto md:max-w-full dark:text-gray text-base md:text-lg mb-5 md:mb-10'>We share our special tools and techniques, and coach you one-on-one <br/>
-        from the beginning to the placement with higher raise.</p>
-    </div>
-    <div className='mx-auto text-center' >
-        <CardsSection/>
-    </div>
-   </div>
+    <>
+      <div className=''>
+        <div className='mx-auto text-center '>
+          <h1 className='max-w-[320px] md:max-w-[739px] mx-auto text-2xl md:text-5xl font-bold text-black leading-tight mb-2 md:mb-4'>Why High Paid Jobs is your ticket <br />to success</h1>
+          <p className='text-[#5C5958] max-w-[270px] mx-auto md:max-w-full dark:text-gray text-base md:text-lg mb-5 md:mb-10'>We share our special tools and techniques, and coach you one-on-one <br />
+            from the beginning to the placement with higher raise.</p>
+        </div>
+        <div className='mx-auto text-center' >
+          <CardsSection />
+        </div>
+        <div className='flex justify-center items-center m-4'>
+          <Button className='btn bg-primary rounded-full text-white m-4 p-4 text-lg'>Book Free Consulatation
+            <Image src={Arrow} alt='arrow'/>
+            <Link href="/" />
+          </Button>
+
+        </div>
+      </div>
 
 
 
@@ -27,7 +33,7 @@ const TicketToSucess = () => {
 
 
 
-  </>
+    </>
   )
 }
 
